71b6396cc96d1b6fc6ed5f50c4ba1d64d3749356
Leah Ewing <leahewing@live.com>
2023-10-22 12:06:11 -0700

    add css for email button

--- static/static.js
+++ static/static.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// const sign_up_link = document.querySelector('#sign-up-link');
+
+// const handleClick = () => {
+//   prompt('Please enter your email:');
+// };
+
+// sign_up_link.addEventListener('click', handleClick);

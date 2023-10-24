0bfa4b72682601cfb3b5beecd9d44ce8ae195c16
Leah Ewing <leahewing@live.com>
2023-10-24 16:26:31 -0700

    user info adding to db from popup

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, flash, session, redirect
+from model import Email, connect_to_db
 from jinja2 import StrictUndefined
 import crud, json
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 
@@ -20,11 +22,20 @@ def calendarView():
 
     return render_template('calendar-view.html')
 
-# @app.route('/', methods = ["POST"]) 
+
+@app.route('/add-email', methods = ["POST"]) 
 def addNewEmail():
     """ Adds new email and redirects user to homepage """
-    
+
+    email_firstname = request.form.get("fname")
+    email_address = request.form.get("email")
+
+    crud.create_email_address(email_firstname, email_address)
+
+    return redirect("/")
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)

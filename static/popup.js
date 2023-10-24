bd4962c53c637b648b3f1e08e4c41d5b0b49b141
Leah Ewing <leahewing@live.com>
2023-10-24 15:05:36 -0700

    basic html/css popup working

---
+++ static/popup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Pop-up window script
+
+const popupButton = document.getElementById("popup-button");
+const popupWindow = document.getElementById("popup-window");
+const closeButton = document.getElementById("close-button");
+
+// Show the pop-up window when the link is clicked
+popupButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+});
+
+// Hide the pop-up window when the close button is clicked
+closeButton.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+});
--- static/static.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// const sign_up_link = document.querySelector('#sign-up-link');
-
-// const handleClick = () => {
-//   prompt('Please enter your email:');
-// };
-
-// sign_up_link.addEventListener('click', handleClick);

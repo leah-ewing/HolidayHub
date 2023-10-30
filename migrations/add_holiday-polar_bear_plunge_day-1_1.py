5905d4c9a206c20b7c0efd1d9e00e4f4dde3d7c1
Leah Ewing <leahewing@live.com>
2023-10-30 11:46:39 -0700

    update migration

--- migrations/add_holiday-polar_bear_plunge_day-1_1.py
+++ migrations/add_holiday-polar_bear_plunge_day-1_1.py
@@ -13,9 +13,13 @@ Holiday:
 holiday_name
 holiday_month
 holiday_date
-holiday_link
-holiday_notes
+holiday_img
+holiday_blurb
 holiday_email 
 """
 
-crud.create_holiday("Polar Bear Plunge Day", 1, 1, "", "", "")
+crud.create_holiday("Polar Bear Plunge Day", 
+                    1, 
+                    1, 
+                    "", 
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac laoreet augue, eget convallis dui. Mauris posuere nulla placerat, convallis quam semper, viverra tortor. Pellentesque eu lectus quis libero pharetra tincidunt. Phasellus sit amet libero ut lectus porttitor posuere. In ut mattis felis, eget auctor ligula. Nam lobortis risus eu egestas egestas. Praesent feugiat mauris in magna euismod, quis lobortis orci faucibus. Aenean eget purus tempus, tempor orci ut, rutrum justo. Vivamus tincidunt vel diam iaculis congue. Vestibulum suscipit dolor in neque condimentum, et finibus nulla ultricies. Phasellus est erat, tincidunt vel placerat eget, efficitur eget metus.", "")

c664842aaf5b263f54693e658ed1161784c38acf
Leah Ewing <leahewing@live.com>
2023-10-30 10:32:36 -0700

    remove semicolons

--- static/js/popup.js
+++ static/js/popup.js
@@ -2,17 +2,17 @@
 
 // Pop-up window script
 
-const popupButton = document.getElementById("popup-button");
-const popupWindow = document.getElementById("popup-window");
-const closeButton = document.getElementById("close-button");
+const popupButton = document.getElementById("popup-button")
+const popupWindow = document.getElementById("popup-window")
+const closeButton = document.getElementById("close-button")
 
 // Show the pop-up window when the link is clicked
 popupButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    popupWindow.style.display = "block";
-});
+    event.preventDefault()
+    popupWindow.style.display = "block"
+})
 
 // Hide the pop-up window when the close button is clicked
 closeButton.addEventListener("click", function() {
-    popupWindow.style.display = "none";
-});
+    popupWindow.style.display = "none"
+})

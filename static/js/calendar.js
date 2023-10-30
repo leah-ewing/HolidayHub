70dce863ef73eda40f58d1572610660083c6b282
Leah Ewing <leahewing@live.com>
2023-10-30 10:30:55 -0700

    fix calendar bug

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -15,59 +15,53 @@ const getFebDays = (year) => {
 }
 
 const generateCalendar = (month, year) => {
-
     let calendar_days = calendar.querySelector('.calendar-days')
     let calendar_header_year = calendar.querySelector('#year')
 
-    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    const firstDayOfMonth = new Date(year, month, 1).getDay()
+    const daysInMonth = new Date(year, month + 1, 0).getDate()
 
     calendar_days.innerHTML = ''
 
-    let currDate = new Date()
-    if (month > 11 || month < 0) month = currDate.getMonth()
-    if (!year) year = currDate.getFullYear()
-
-    let curr_month = `${month_names[month]}`
-    month_picker.innerHTML = curr_month
     calendar_header_year.innerHTML = year
+    month_picker.innerHTML = month_names[month]
 
-    // Get first day of month
-    let first_day = new Date(year, month, 1)
+    for (let i = 0; i < firstDayOfMonth; i++) {
+        // Add empty cells for days before the 1st day of the month
+        const emptyDay = document.createElement('div')
+        emptyDay.classList.add('calendar-empty-day')
+        calendar_days.appendChild(emptyDay)
+    }
 
-    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-        let day = document.createElement('div')
-        if (i >= first_day.getDay()) {
-            day.classList.add('calendar-day-hover')
-            day.innerHTML = i - first_day.getDay() + 1
-            day.innerHTML += `<span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>`
-            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
-                day.classList.add('curr-date')
-            }
-            calendar_days.appendChild(day)
+    for (let day = 1; day <= daysInMonth; day++) {
+        const dayElement = document.createElement('div')
+        dayElement.classList.add('calendar-day-hover')
+        dayElement.innerHTML = day;
 
-            day.addEventListener('click', (evt) => {   
-                evt.preventDefault();
+        if (day === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
+            dayElement.classList.add('curr-date')
+        }
 
-                let date_picked = String(parseInt(day.innerHTML))
-                let month_picked = String(month_picker.innerHTML)
-                let url = `day-picker/${month_picked}/${date_picked}`
+        calendar_days.appendChild(dayElement)
 
-                fetch(url)
+        dayElement.addEventListener('click', (evt) => {
+            evt.preventDefault();
+            const date_picked = String(day);
+            const month_picked = month_names[month];
+            const url = `day-picker/${month_picked}/${date_picked}`
+
+            fetch(url)
                 .then(response => {
                     if (response.status === 200) {
-                        window.location.href = url;
+                        window.location.href = url
                     } else {
-                        console.error('Error:', response.status);
+                        console.error('Error:', response.status)
                     }
                 })
                 .catch(error => {
-                    console.error('Error:', error);
-                });
-            }) 
-        }
+                    console.error('Error:', error)
+                })
+        })
     }
 }
 

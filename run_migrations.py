0a9131f7a50da05c53f7bd7a018207d502474385
Leah Ewing <leahewing@live.com>
2023-11-06 19:42:20 -0800

    remove comments

--- run_migrations.py
+++ run_migrations.py
@@ -5,5 +5,4 @@ directory = 'migrations'
 for file in os.listdir(directory):
     file_name = os.path.join(directory, file)
     if os.path.isfile(file_name):
-        # print(file_name)
         os.system(f"python3 {file_name}")

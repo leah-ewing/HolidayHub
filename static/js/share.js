ec2c51ff1bd4fb082898776964520a7b9d816397
Leah Ewing <leahewing@live.com>
2023-11-08 12:05:26 -0800

    add share functionality

--- static/js/share.js
+++ static/js/share.js
@@ -1,7 +1,18 @@
 'use strict';
 
+// Share button script
+
 let shareButton = document.getElementById("share-button")
 
-shareButton.addEventListener('click', event => {
-    alert('test')
-});
+shareButton.addEventListener("click", async () => {
+    if (navigator.share) {
+        try {
+            await navigator.share({ title: "Example Page", url: "" })
+            console.log("Data was shared successfully")
+        } catch (err) {
+            console.error("Share failed:", err.message)
+        }
+    } else {
+        alert("test")
+    }
+})

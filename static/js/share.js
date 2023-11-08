bd3ac50703bbb71c1467fed22b7dc0cc2d624863
Leah Ewing <leahewing@live.com>
2023-11-08 12:25:48 -0800

    commit chrome extension logic

--- static/js/share.js
+++ static/js/share.js
@@ -2,7 +2,9 @@
 
 // Share button script
 
-let shareButton = document.getElementById("share-button")
+const shareButton = document.getElementById("share-button")
+const sharePopUpWindow = document.getElementById("share-popup-window")
+const closeButtonSharePopUpWindow = document.getElementById("share-close-button")
 
 shareButton.addEventListener("click", async () => {
     if (navigator.share) {
@@ -13,6 +15,11 @@ shareButton.addEventListener("click", async () => {
             console.error("Share failed:", err.message)
         }
     } else {
-        alert("test")
+        // alert("test")
+        sharePopUpWindow.style.display = "block"
     }
+})
+
+closeButtonSharePopUpWindow.addEventListener("click", function() {
+    sharePopUpWindow.style.display = "none"
 })

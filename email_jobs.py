931ac6d0fec414b9338c571d000dd053a8d3144a
Leah Ewing <leahewing@live.com>
2023-11-14 17:12:19 -0800

    add email_jobs.py shell

---
+++ email_jobs.py
@@ -0,0 +1,12 @@
+
+def send_welcome_email_upon_signup():
+    """ When a new email is inserted into the database, a welcome email is sent """
+    pass
+
+def send_daily_holiday_email():
+    """ Sends a holiday email once a day to emails that are opted in """
+    pass
+
+def remove_opted_out_emails_from_db():
+    """ Checks 2x a day for opted out emails and removeds them from the database """
+    pass

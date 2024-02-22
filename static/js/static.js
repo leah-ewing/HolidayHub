d7d3705804543aba950fed95bc1475edb5b92587
Leah Ewing <leahewing@live.com>
2024-02-21 21:35:04 -0800

    create animated title on homepage

--- static/js/static.js
+++ static/js/static.js
@@ -1,1 +1,47 @@
 'use strict';
+
+// Animated title script
+
+async function init () {
+    const node = document.querySelector("#type-text")
+    
+    await sleep(1000)
+    node.innerText = ""
+
+    const title_words = ['Celebrate', 'Gather', 'Party', 'Dance', 'Laugh', 'Chat', 'Love']
+
+    while (true) {
+        for (let word of title_words) {
+            await node.type(word)
+            await sleep(2000)
+            await node.delete(word)
+        }
+    }
+  }
+
+  const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+  
+  class TypeAsync extends HTMLSpanElement {
+    get typeInterval () {
+      const randomMs = 100 * Math.random()
+      return randomMs < 50 ? 10 : randomMs
+    }
+    
+    async type (text) {
+      for (let character of text) {
+        this.innerText += character
+        await sleep(this.typeInterval)
+      }
+    }
+    
+    async delete (text) {
+      for (let character of text) {
+        this.innerText = this.innerText.slice(0, this.innerText.length -1)
+        await sleep(this.typeInterval)
+      }
+    }
+  }
+  
+  customElements.define('type-async', TypeAsync, { extends: 'span' })
+  
+  init()

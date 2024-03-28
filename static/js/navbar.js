879c69e06ff90e394c702bc0cd8bf02e2aecb780
Leah Ewing <leahewing@live.com>
2024-03-28 10:52:58 -0700

    fix navbar transition timing

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -6,7 +6,7 @@ let lastScrollTop
 window.addEventListener("scroll", function() {
   let position = window.pageYOffset || document.documentElement.scrollTop
 
-  if (position > lastScrollTop)  {
+  if (position > lastScrollTop && document.documentElement.scrollTop > 50)  {
     nav.style.opacity = 0 // hide the nav-bar when going down
   } else {
     nav.style.opacity = 1 // display the nav-bar when going up

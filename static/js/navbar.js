9c456a3bb5b3469e4107c1ce29499a4cf343027e
Leah Ewing <leahewing@live.com>
2024-03-28 10:50:32 -0700

    tweak navbar transition

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,16 +1,16 @@
 'use strict';
 
 let nav = document.querySelector('#navbar')
-      let lastScrollTop
+let lastScrollTop
 
-      window.addEventListener("scroll", function() {
-        var position = window.pageYOffset || document.documentElement.scrollTop;
-        if (position > lastScrollTop) 
-          nav.style.opacity = 0 // hide the nav-bar when going down
-        else 
-          nav.style.opacity = 1 // display the nav-bar when going up
-        
-        lastScrollTop = position
+window.addEventListener("scroll", function() {
+  let position = window.pageYOffset || document.documentElement.scrollTop
 
-        nav.style.top = `${position}px` // set the position of the nav-bar to the current scroll
-      }, false)
+  if (position > lastScrollTop)  {
+    nav.style.opacity = 0 // hide the nav-bar when going down
+  } else {
+    nav.style.opacity = 1 // display the nav-bar when going up
+  } 
+
+  lastScrollTop = position
+})

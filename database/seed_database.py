5bfbf687e53b231db946e2b00ba9b2d9c363fd7c
Leah Ewing <leahewing@live.com>
2024-04-04 17:31:16 -0700

    uncomment code

--- database/seed_database.py
+++ database/seed_database.py
@@ -13,7 +13,6 @@ from crud import create_month, create_email_address
 os.system('dropdb holidaydb')
 os.system('createdb holidaydb')
 
-# model.connect_to_db(server.app)
 model.db.create_all()
 
 """ Seed Months """

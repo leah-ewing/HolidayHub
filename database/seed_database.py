a2ac9bb1084aed4276920610f88e7004dede63a7
Leah Ewing <leahewing@live.com>
2024-04-23 19:09:52 -0700

    update command

--- database/seed_database.py
+++ database/seed_database.py
@@ -19,8 +19,8 @@ os.environ['PGPASSWORD'] = DB_PASSWORD
 # subprocess.run(['createdb', '-h', 'localhost', '-p', '5432', '-U', 'leahewing', DB_NAME], check=True)
 
 # os.system(f'dropdb {DB_NAME}')
-# os.system(f'dropdb holidaydb_prod')
-os.system(f'createdb -h localhost -p 5432 -U {DB_USERNAME} -W {DB_PASSWORD} {DB_NAME}')
+os.system(f'createdb holidaydb_prod')
+# os.system(f'createdb -h localhost -p 5432 -U {DB_USERNAME} -W {DB_PASSWORD} {DB_NAME}')
 # os.system(f'createdb -h localhost -p 5432 -U postgres -W test holidaydb_prod')
 
 db.create_all()
